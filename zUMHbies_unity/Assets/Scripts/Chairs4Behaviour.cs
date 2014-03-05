0465bdf37dbc132111db7ff1f51fc62c67f0dc32
Héctor Barreras Almarcha <hbaelx@gmail.com>
2014-03-05 23:15:53 +0100

    - Modified bathroom model - Readded all modified models to unity - New algorithmic substances for marble - Some files rearranged - Gameobjects can now be broken apart (view chairs4-interaction(E)) - Chairs4 model optimized - Minor modifications

---
+++ zUMHbies_unity/Assets/Scripts/Chairs4Behaviour.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Chairs4Behaviour : MonoBehaviour, IInteractive
+{
+
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+
+		public void Activate ()
+		{
+				Break ();
+		}
+
+		public void Break ()
+		{
+				collider.enabled = false;
+				int t_initialChildren = transform.childCount;
+				for (int i=0; i<t_initialChildren; i++) {
+						ContainedPhysicsBehaviour behaviour = transform.GetChild (0).GetComponent<ContainedPhysicsBehaviour> ();
+					
+						if (behaviour != null) {
+								behaviour.SetFree ();
+						}
+						
+				}
+				//Called in case there are some remaining objects inside the hierarchy
+				transform.DetachChildren ();
+				Destroy (gameObject);
+		}
+}
---
+++ zUMHbies_unity/Assets/Scripts/ContainedPhysicsBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ContainedPhysicsBehaviour : MonoBehaviour
+{
+
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		public void SetFree ()
+		{
+				transform.parent = null;
+
+				if (rigidbody != null)
+						rigidbody.isKinematic = false;
+				else
+						print ("Warning: GameObject " + name + " does not have a rigidbody attached");
+
+				if (collider != null)
+						collider.enabled = true;
+				else
+						print ("Warning: GameObject " + name + " does not have a collider attached");
+		}
+}
