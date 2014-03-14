304a42171090e87852b7795cdf57d6a48092ce93
vicenteloco <vicenteq1@hotmail.es>
2014-03-14 02:05:50 +0100

    New folder Tap: tap system

---
+++ zUMHbies_unity/Assets/Tap/DestroyTap_Water.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyTap_Water : MonoBehaviour {
+	private int TimeDestroy=9;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Destroy (this.gameObject, TimeDestroy);
+	}
+}
---
+++ zUMHbies_unity/Assets/Tap/Tap_Water.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tap_Water: GeneralInteractiveBehaviour{
+	
+	// C#
+	// Instantiates a prefab in a circle
+	public Transform prefab;
+	public Transform Empty;
+	public int ModifierTime=6;
+	private int i=0;
+	public override void _Activate (){
+	if (i<1){
+	Instantiate(prefab, Empty.position, Quaternion.identity);
+			StartCoroutine(TestCoroutine());
+			i++;
+	}
+			
+	}
+	IEnumerator TestCoroutine(){
+						yield return new WaitForSeconds (ModifierTime);
+						i--;
+	}
+	}
