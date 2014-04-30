2d8d912d8507b3634eaac6590937af88e768fcf4
vicenteloco <vicenteq1@hotmail.es>
2014-04-30 03:02:32 +0200

    NewModels for human and zombie. zombieGiant

--- zUMHbies_unity/Assets/Scripts/Zombies/ZombieBasicBehaviour.cs
+++ zUMHbies_unity/Assets/Scripts/Zombies/ZombieBasicBehaviour.cs
@@ -3,173 +3,197 @@ using System.Collections;
 
 public class ZombieBasicBehaviour : MonoBehaviour, IKillable, ISwitchedByExtTrigger
 {
-		public static PlayerReferences PlayerRefs;
-
-		public Transform HeadTransform;
-
-		public int MaxHealth;
-		public float WanderSpeed;
-		public float RunSpeed;
-		public float ReactionTime;
-		public float WaitUntilRunTime;
-		public float SecondsBetweenRaycasts;
-
-		protected float	health;
-		protected NavMeshAgent myNavMeshAgent;
-		protected Vector3 myDestination;
-
-		protected bool busy;
-		protected bool chasingPlayer;
-
-		//IKillable members
-		public int _MaxHealth {
-				get {
-						return MaxHealth;
-				}
-				set {
-						MaxHealth = value;
-				}
-		}
-
-		public float _Health {
-				get {
-						return health;
-				}
-
-				set {
-						health = value;
-				}
-		}
-
-		public void _TakeDamage (float a_damage, Vector3 a_hitPoint)
-		{
-				print ("ayayayay");
-				health -= a_damage;
-
-				if (health <= 0)
-						_Die ();
-		}
-
-		public void _Die ()
-		{
-				health = 0; //In case it was negative -not that important anyway-
-
-				//Scream or something
-		}
-		//END OF IKillable MEMBERS
-
-		//ISwitchedByExtTrigger members
-
-		public void _TriggerEnterSwitch (int a_index, Collider a_collider)
-		{
-				
-		}
-
-		public void _TriggerStaySwitch (int a_index, Collider a_collider)
-		{
-				switch (a_index) {
-				case 0: //Vision trigger
-						if (a_collider == PlayerRefs.Player.collider)
-								checkPlayerVisibility ();
-						break;
-				}
-		}
-
-		public void _TriggerExitSwitch (int a_index, Collider a_collider)
-		{
-
-		}
-		//END OF ISwitchedByExtTrigger MEMBERS
-
-
-		// Use this for initialization
-		void Start ()
-		{
-				PlayerRefs = GameObject.FindGameObjectWithTag (Tags.GAME_CONTROLLER).GetComponent<PlayerReferences> ();
-				health = MaxHealth;
-				myNavMeshAgent = GetComponent<NavMeshAgent> ();
-				myDestination = transform.position;
-		}
+	public static PlayerReferences PlayerRefs;
 	
-		// Update is called once per frame
-		void FixedUpdate ()
-		{
-				//Wander around
+	public Transform HeadTransform;
+	
+	public int MaxHealth;
+	public float WanderSpeed;
+	public float RunSpeed;
+	public float ReactionTime;
+	public float WaitUntilRunTime;
+	public float SecondsBetweenRaycasts;
+	
+	protected float	health;
+	protected NavMeshAgent myNavMeshAgent;
+	protected Vector3 myDestination;
+	
+	public bool busy;
+	public bool chasingPlayer;
+	public bool Damage;
+
+	public float Stun;
+    public Transform[] TargetDestination;
+
+	protected int Destination=0;
+	protected float dist;
+	//IKillable members
+	public int _MaxHealth {
+		get {
+			return MaxHealth;
 		}
-
-		void OnTriggerStay (Collider a_collider)
-		{
-				//Zombie gets hit by light that could be being managed by player
-				if (!busy && a_collider.tag == Tags.ATTRACTIVE_LIGHT) {
-
-						//Is the light managed by a player?
-						IPickable t_light = a_collider.gameObject.Ext_GetClosestBehaviourWithInterfaceInHierarchy<IPickable> ();
-						if (t_light != null && t_light._Equiped == true) {
-
-								//Direct vision with player?
-								if (gameObject.Ext_DirectRay (HeadTransform.position, PlayerRefs.Player.transform.position, PlayerRefs.PlayerCollider)) {
-										busy = true;
-										StopCoroutine ("attentionTowards"); //Stops all previous attention
-										StartCoroutine ("attentionTowards", a_collider.transform.parent.position); //Real center of the light is in the parent of the light
-								}
-						}
-				}
+		set {
+			MaxHealth = value;
 		}
-
-		void checkPlayerVisibility ()
-		{
-				//Already chasing the player?
-				if (chasingPlayer)
-						return; //Yes, return
-
-				//Direct vision with player? Go chase that little bitch
-				if (gameObject.Ext_DirectRay (HeadTransform.position, PlayerRefs.Player.transform.position, PlayerRefs.Player.collider)) {
-						StartCoroutine (chasePlayer ());
-				}
-		}
-
-		protected IEnumerator attentionTowards (Vector3 a_destination)
-		{
-				//Act weird
-				yield return new WaitForSeconds (ReactionTime);
-
-				setDestination (a_destination, 0.05f); //Only able to look at the target, barely translates
-				yield return new WaitForSeconds (WaitUntilRunTime);
-
-				run ();
+	}
+	
+	public float _Health {
+		get {
+			return health;
 		}
 		
-		protected void setDestination (Vector3 a_destination, float a_newSpeed)
-		{
-				myDestination = a_destination;
-				myNavMeshAgent.SetDestination (a_destination);
-				myNavMeshAgent.speed = a_newSpeed;
+		set {
+			health = value;
 		}
-
-		protected void run ()
-		{
-				myNavMeshAgent.speed = RunSpeed;
+	}
+	
+	public void _TakeDamage (float a_damage, Vector3 a_hitPoint)
+	{
+		print ("ayayayay");//falta que cuando pegas con las manos se llame a esta funcion
+		Damage = true;
+		StartCoroutine(TestCoroutine());
+		health -= a_damage;
+		if (health <= 0)
+			_Die ();
+	}
+	
+	public void _Die ()
+	{
+		health = 0; //In case it was negative -not that important anyway-
+		
+		//Scream or something
+	}
+	//END OF IKillable MEMBERS
+	
+	//ISwitchedByExtTrigger members
+	
+	public void _TriggerEnterSwitch (int a_index, Collider a_collider)
+	{
+		
+	}
+	
+	public void _TriggerStaySwitch (int a_index, Collider a_collider)
+	{
+		switch (a_index) {
+		case 0: //Vision trigger
+			if (a_collider == PlayerRefs.Player.collider)
+				checkPlayerVisibility ();
+			break;
 		}
-
-		protected IEnumerator chasePlayer ()
-		{
+	}
+	
+	public void _TriggerExitSwitch (int a_index, Collider a_collider)
+	{
+		
+	}
+	//END OF ISwitchedByExtTrigger MEMBERS
+	
+	
+	// Use this for initialization
+	void Start ()
+	{
+		PlayerRefs = GameObject.FindGameObjectWithTag (Tags.GAME_CONTROLLER).GetComponent<PlayerReferences> ();
+		health = MaxHealth;
+		myNavMeshAgent = GetComponent<NavMeshAgent> ();
+		myDestination = transform.position;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+		//Wander around
+		/* 
+		dist= Vector3.Distance (TargetDestination[Destination].position,transform.position);
+		if (busy == false && chasingPlayer == false&&Damage==false) {
+			if(Destination<(TargetDestination.Length-1)){
 				busy = true;
 				chasingPlayer = true;
-				run ();
-
-				while (true) {
-						//Is the player still visible?
-						if (gameObject.Ext_DirectRay (HeadTransform.position, PlayerRefs.Player.transform.position, PlayerRefs.Player.collider)) {
-								//Update target
-								myNavMeshAgent.SetDestination (PlayerRefs.FeetTransform.position);
-								yield return new WaitForSeconds (SecondsBetweenRaycasts);
-						} else {
-								//Player lost, zombie will continue running towards target, but will update the target no more
-								break;
-						}
-				}
-
-				chasingPlayer = false;
-				busy = false;
+			myNavMeshAgent.destination=TargetDestination[Destination].position;
+			}
 		}
+		*/
+	}
+	
+	void OnTriggerStay (Collider a_collider)
+	{
+		//Zombie gets hit by light that could be being managed by player
+		if (!busy && a_collider.tag == Tags.ATTRACTIVE_LIGHT) {
+			
+			//Is the light managed by a player?
+			IPickable t_light = a_collider.gameObject.Ext_GetClosestBehaviourWithInterfaceInHierarchy<IPickable> ();
+			
+			if (t_light != null && t_light._Equiped == true) {
+				
+				//Direct vision with player?
+				//if (gameObject.Ext_DirectRay (HeadTransform.position, PlayerRefs.Player.transform.position, PlayerRefs.PlayerCollider)) {
+				busy = true;
+				StopCoroutine ("attentionTowards"); //Stops all previous attention
+				StartCoroutine ("attentionTowards", a_collider.transform.parent.position); //Real center of the light is in the parent of the light
+				//}
+			}
+		}
+	}
+	
+	void checkPlayerVisibility ()
+	{
+		//Already chasing the player?
+		if (chasingPlayer)
+			return; //Yes, return
+		
+		//Direct vision with player? Go chase that little bitch
+		if (gameObject.Ext_DirectRay (HeadTransform.position, PlayerRefs.Player.transform.position, PlayerRefs.Player.collider)) {
+			StartCoroutine (chasePlayer());
+		}
+	}
+	
+	protected IEnumerator attentionTowards (Vector3 a_destination)
+	{
+		//Act weird
+		yield return new WaitForSeconds (ReactionTime);
+		
+		setDestination (a_destination, 0.05f); //Only able to look at the target, barely translates
+		yield return new WaitForSeconds (WaitUntilRunTime);
+		
+		run ();
+	}
+	
+	protected void setDestination (Vector3 a_destination, float a_newSpeed)
+	{
+		myDestination = a_destination;
+		myNavMeshAgent.SetDestination (a_destination);
+		myNavMeshAgent.speed = a_newSpeed;
+	}
+	
+	protected void run ()
+	{
+		myNavMeshAgent.speed = RunSpeed;
+	}
+	
+	protected IEnumerator chasePlayer ()
+	{
+		busy = true;
+		chasingPlayer = true;
+		run ();
+		
+		while (true) {
+			//Is the player still visible?
+			if (gameObject.Ext_DirectRay (HeadTransform.position, PlayerRefs.Player.transform.position, PlayerRefs.Player.collider)&&Damage==false) {
+				//Update target
+				myNavMeshAgent.SetDestination (PlayerRefs.FeetTransform.position);
+				yield return new WaitForSeconds (SecondsBetweenRaycasts);
+			} else {
+				//Player lost, zombie will continue running towards target, but will update the target no more
+				break;
+			}
+		}
+		
+		chasingPlayer = false;
+		busy = false;
+	}
+	protected IEnumerator TestCoroutine(){
+		myNavMeshAgent.Stop ();
+		yield return new WaitForSeconds (Stun);
+		Damage = false;
+		StartCoroutine (chasePlayer ());
+	}
 }
---
+++ zUMHbies_unity/Assets/Zombie/Zombie_Animation.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Zombie_Animation : MonoBehaviour {
+	// Use this for initialization
+	protected bool Detected;
+	public Animator Animation_Zombie;
+	public int Number_Aleatori;
+	//Mask
+	public bool AttackOut;
+	//Distance PlayerZombie
+	public GameObject Player;
+	public float Length;
+	public bool AttackIn;
+	protected bool Damage;
+	public float SizeCharacterController;
+	public float PositionControllerColliderCenter;
+	public float SizeCapsuleCollider;
+	public float PositionCapsuleColliderCenter;
+	public float RadiusCapsule;
+	CharacterController Controller;
+	protected CapsuleCollider Collider;
+	public GameObject CapsuleCollider;
+	protected float AxisH;
+	protected float AxisV;
+	void Start () {
+		InvokeRepeating ("Randomizar",1,4);
+		Animation_Zombie = GetComponent<Animator> ();
+		Animation_Zombie.SetLayerWeight (1, 2f);
+		Animation_Zombie.SetLayerWeight (2, 2f);
+	    Controller = GetComponent<CharacterController>();
+		Collider = CapsuleCollider.gameObject.transform.GetComponent<CapsuleCollider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Detected = this.gameObject.GetComponent<ZombieBasicBehaviour> ().chasingPlayer;
+		if (Detected == true) {
+			Animation_Zombie.SetBool ("look",true);
+				} 
+		else {
+			Animation_Zombie.SetBool ("look",false);
+				}
+		if (Number_Aleatori >= 5&&Detected == true) {
+			Animation_Zombie.SetBool ("floor",true);
+		}
+		if (Number_Aleatori < 5&&Detected == true) {
+			Animation_Zombie.SetBool ("floor",false);
+		}
+		//Mask
+		if (AttackOut == true) {
+						Animation_Zombie.SetBool ("AttackOut", true);
+				} else {
+						Animation_Zombie.SetBool ("AttackOut", false);
+				}
+		//Distance to Player and figth
+		Length=Vector3.Distance (this.gameObject.transform.position, Player.transform.position);
+		if (Length <= 2&&Length>1) {
+						AttackOut = true;
+			            AttackIn=false;
+			            Animation_Zombie.SetBool ("AttackIn", false);
+				} 
+		else {
+			AttackOut=false;
+				}
+		if (Length <= 1) {
+			AttackIn=true;
+			Animation_Zombie.SetBool ("AttackIn", true);
+				}
+		//Lantern
+		AxisH=Input.GetAxis ("Horizontal");
+		AxisV=Input.GetAxis ("Vertical");
+		if (Length <= 3.5 && (AxisH == 1 || AxisV == 1)) {
+			//Debe de llamar a protected IEnumerator chasePlayer ()
+		}
+		//Damage
+		Damage = this.gameObject.GetComponent<ZombieBasicBehaviour> ().Damage;
+		if (Damage == true) {
+						Animation_Zombie.SetBool ("Damage", true);
+			            Animation_Zombie.SetBool ("AttackOut", false);
+				} else {
+						Animation_Zombie.SetBool ("Damage", false);			           
+				}
+		//ColliderSize
+		if (Number_Aleatori >= 5 && Detected == true) {
+						Controller.height = SizeCharacterController;
+			            Controller.center = new Vector3 (0, PositionControllerColliderCenter, 0);
+			            Controller.radius = RadiusCapsule;
+			            Collider.height = SizeCapsuleCollider;
+			            Collider.center = new Vector3 (0, PositionCapsuleColliderCenter, 0);
+			            Collider.radius=RadiusCapsule;
+		} else {
+			            Controller.height = 0.8f;
+			            Controller.center = new Vector3 (0, 0, 0);
+			            Collider.height = 0.3f;
+			            Collider.center = new Vector3 (0, 0, 0);
+			            Controller.radius = 0.6f;
+			            Collider.radius=0.6f;
+		}
+	}
+	void Randomizar(){
+		if (AttackIn == false&&Damage==false) {
+						Number_Aleatori = Random.Range (1, 10);
+				}
+		}
+}
