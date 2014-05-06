58dbab619c3cebf4d751b74caba67c8370c19950
Miguel Catalan Bañuls <miguel.catalan.banuls@gmail.com>
2014-05-06 22:44:26 +0200

    - Implemented background music script. Need to add fadein and fadeout controllers.

--- zUMHbies_unity/Assets/Scripts/GlobalSoundControl.cs
+++ zUMHbies_unity/Assets/Scripts/GlobalSoundControl.cs
@@ -5,11 +5,37 @@ public class GlobalSoundControl : MonoBehaviour
 {
 		public AudioSource[] AudioSources;
 
+		public AudioClip[] AtmosphereClips;
+
+		public AudioClip[] TensionClips;
+
 		public AudioClip NoMercy;
+
+		private int currentAudioSource;
+
+		private AudioClip[] CurrentTheme;
+
+		void Start () 
+		{
+			currentAudioSource = 0;
+			ChangeToAtmosphere();
+			PlayNextClip(0);
+		}
+	
+		void Update()
+		{
+			if(!AudioSources[currentAudioSource].isPlaying) 
+			{
+				ChangeToTension();
+				PlayNextClip();
+				
+			}
+		}
 
 		public void PlayDeathSound ()
 		{
 				StartPlaying (NoMercy);
+				
 		}
 
 		public void StartPlaying (AudioClip a_audioClip, bool a_loop = false)
@@ -22,5 +48,58 @@ public class GlobalSoundControl : MonoBehaviour
 								return;
 						}
 				}
+		}
+
+		private void PlayNextClip ()
+		{
+			SetCurrentAudioSource();
+			AudioSources[currentAudioSource].clip = CurrentTheme[Random.Range(0,AtmosphereClips.Length)];
+			AudioSources[currentAudioSource].loop = false;
+			AudioSources[currentAudioSource].Play();
+		}
+
+		private void PlayNextClip (int clip)
+		{
+			SetCurrentAudioSource();
+			AudioSources[currentAudioSource].clip = CurrentTheme[clip];
+			AudioSources[currentAudioSource].loop = false;
+			AudioSources[currentAudioSource].Play();
+		}
+
+		private void SetCurrentAudioSource ()
+		{
+			if(currentAudioSource == 0)
+			{
+				currentAudioSource = 1;
+			}
+			else
+			{
+				currentAudioSource = 0;
+			}
+		}
+
+		private void FadeIn (AudioSource a_clip)
+		{
+
+		}
+
+		private void FadeOut (AudioSource a_clip)
+		{
+			
+		}
+
+		private void ChangeAudioSource()
+		{
+			
+		}
+
+		public void ChangeToTension ()
+		{
+			CurrentTheme = TensionClips;
+		}
+
+		public void ChangeToAtmosphere ()
+		{
+			CurrentTheme = AtmosphereClips;
 		}
 }
